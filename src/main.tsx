cd05e2d0693482e169ff4505b7ac9d5b4d7263ce
Diogo Esteves <diogo.esteves.goncalves@gmail.com>
2024-08-30 18:31:17 +0100

    Integrate React Query and update global styles
    Added React Query support by incorporating QueryClientProvider around AppRouter in main.tsx. Updated global styles in styles.css to improve font rendering and set default body dimensions and behavior for better UI consistency.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import App from './app/app';
+import { AppRouter } from '@vidoso-fe-task/routes';
+import { ROOT_ELEMENT_ID } from '@vidoso-fe-task/constants';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById(ROOT_ELEMENT_ID) as HTMLElement
 );
+
 root.render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <AppRouter />
+    </QueryClientProvider>
   </StrictMode>
 );

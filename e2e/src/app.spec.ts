487a17653db7ca762d8c0663e6690cad2a5e35a1
Diogo Esteves <diogo.esteves.goncalves@gmail.com>
2024-08-31 17:52:35 +0100

    Add dummy testes to check CI

--- e2e/src/app.spec.ts
+++ e2e/src/app.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Dummy test', async ({ page }) => {
+test('dummy test', async ({ page }) => {
   await page.goto('/');
 
   expect(await page.locator('nav').innerText()).toContain('Users');

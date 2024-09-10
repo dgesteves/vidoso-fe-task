5e3ddf3920b881be3709608478256e9ebf9d11da
Diogo Esteves <diogo.esteves.goncalves@gmail.com>
2024-09-10 16:14:38 +0100

    Introduces STALE_TIME value

--- src/constants/src/lib/pagination.ts
+++ src/constants/src/lib/pagination.ts
@@ -3,3 +3,4 @@ export const ELEMENTS_PER_PAGE = 10;
 export const INDEX_AFTER_SLASH = 1;
 export const NEXT_BUTTON_TEXT = 'Next';
 export const PREV_BUTTON_TEXT = 'Prev';
+export const STALE_TIME = 5000;
